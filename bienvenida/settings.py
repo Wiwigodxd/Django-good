1dd913aa5a3ae3837996f5fe32728455173e5806
Wiwigodxd <edwin.sanchez06@inacapmail.cl>
2025-10-30 16:04:26 -0300

    feat : nose

--- bienvenida/settings.py
+++ bienvenida/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-d4glj&86idy34f$!k8+uv_c9y1j$x^$^95a^gmr*5n7e_%(=$b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".onrender.com","127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
